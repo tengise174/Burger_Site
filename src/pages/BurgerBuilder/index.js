6aae5d6516d2c40d4a9b7e103757fe9fb4c68dc2
tengise174 <117653225+tengise174@users.noreply.github.com>
2024-07-28 01:26:28 -0500

    first commit

---
+++ src/pages/BurgerBuilder/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class BurgerBuilder extends Component {
+    render() {
+        return (
+            <div>
+                <div>Hachirtai talh</div>
+                <div>Ortsnii udirdlaga</div>
+            </div>
+        )
+    }
+};
+
+export default BurgerBuilder;
