3a910f99002bee337f402a953f6c76f8fb249684
tengise174 <117653225+tengise174@users.noreply.github.com>
2024-07-29 02:08:13 -0500

    dynamic build

--- src/components/Burger/index.js
+++ src/components/Burger/index.js
@@ -2,16 +2,31 @@ import React from "react";
 import BurgerIngredient from "../BurgerIngredient";
 import css from "./style.module.css";
 
-const Burger = () => (<div className={css.Burger}>
+const Burger = (props) => {
 
-    <BurgerIngredient type="bread-top" />
-    <BurgerIngredient type="salad" />
-    <BurgerIngredient type="bacon" />
-    <BurgerIngredient type="cheese" />
-    <BurgerIngredient type="meat" />
-    <BurgerIngredient type="meat" />
-    <BurgerIngredient type="bread-bottom" />
 
-</div>)
+    const items = Object.entries(props.orts);
+
+    let content = [];
+    items.map(el => {
+        for (let i = 0; i < el[1]; i++)
+            content.push(<BurgerIngredient key={`${el[0]}${i}`} type={el[0]} />)
+        return 0;
+    });
+
+
+
+    if (content.length === 0)
+        content = <p>Hachirtai talhnii ortsiig songono uu</p>;
+
+    return (
+        <div className={css.Burger}>
+
+            <BurgerIngredient type="bread-top" />
+            {content}
+            <BurgerIngredient type="bread-bottom" />
+
+        </div>)
+};
 
 export default Burger;
--- src/pages/BurgerBuilder/index.js
+++ src/pages/BurgerBuilder/index.js
@@ -2,10 +2,20 @@ import React, { Component } from "react";
 import Burger from "../../components/Burger";
 
 class BurgerBuilder extends Component {
+
+    state = {
+        ingredients: {
+            salad: 0,
+            cheese: 0,
+            bacon: 0,
+            meat: 0
+        }
+    };
+
     render() {
         return (
             <div>
-                <Burger />
+                <Burger orts={this.state.ingredients} />
                 <div>Ortsnii udirdlaga</div>
             </div>
         )
