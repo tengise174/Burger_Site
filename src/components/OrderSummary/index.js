d968d9a2a6e5229aa954264ec9227ef0225be075
tengise174 <117653225+tengise174@users.noreply.github.com>
2024-08-06 02:03:15 -0500

    modal window

--- src/components/OrderSummary/index.js
+++ src/components/OrderSummary/index.js
@@ -7,7 +7,7 @@ const OrderSummary = props => {
             <p>Таны сонгосон орцууд: </p>
             <ul>
                 {Object.keys(props.ingredients).map(el =>
-                    <li>{props.ingredientsNames[el]}: {props.ingredients[el]}</li>)}
+                    <li key={el}>{props.ingredientsNames[el]}: {props.ingredients[el]}</li>)}
             </ul>
 
             <p>Цаашаа үргэлжлүүлэх үү?</p>
